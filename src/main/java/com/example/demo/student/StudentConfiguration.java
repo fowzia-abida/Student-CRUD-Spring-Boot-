07313786b5d982c573b8fd7a02171e98224e8c88
Abida <fowzia.abida21@gmail.com>
2021-04-28 01:52:31 +0200

    First commit

---
+++ src/main/java/com/example/demo/student/StudentConfiguration.java
@@ -0,0 +1,38 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.*;
+
+@Configuration
+public class StudentConfiguration {
+    @Bean
+    CommandLineRunner commandLineRunner (StudentRepository repository)
+    {
+        return args ->{
+
+
+            Student fowzia_abida = new Student(
+                    "Fowzia Abida",
+                    "fowzia@gmail.com",
+                    LocalDate.of(2000, APRIL, 25)
+            );
+
+            Student alex = new Student(
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2004, APRIL, 25)
+            );
+            repository.saveAll(
+                    List.of(fowzia_abida, alex)
+            );
+
+        };
+    }
+}
